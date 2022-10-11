4feca20d13d8c1f081c4bb975d93fb423024064b
Varishdan Kumariah <varishdan.kumariah@kcl.ac.uk>
2022-10-11 21:05:12 +0100

    done

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
+
     list = []
+    x = 2
+    while len(list) < number_of_primes:
+        flag = False
+        for i in range (2,x):
+            if i!=x:
+                if x%i==0:
+                    flag = True
+            else:
+                pass
+        if flag==False:
+            list.append(x)
+
+        x = x + 1
     return list
